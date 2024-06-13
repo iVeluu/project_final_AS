64440cf165667388ab09c0757bb898f1f009f2c3
iVeluu <velualex10@gmail.com>
2024-06-13 17:41:38 -0600

    nnnnn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -113,7 +113,7 @@ export default function Home() {
                     {" "}
                     Nombre: {""}
                     <span className="font-bold  text-white">
-                      Alejandro Velazquez Luuuuu jjj jshs
+                      Alejandro Velazquez Luuuuu jjj jshs nnnnn
                     </span>
                   </p>
                   <p className="font-black text-1xl text-tomato">
